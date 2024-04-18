33b933cb315a9c62d3359008e60cf03de7fa8654
Alexandr <93984557+A1ExAnDr1@users.noreply.github.com>
2024-04-18 18:23:48 +0300

    ci/cd+

--- tests/integ/integ_test.py
+++ tests/integ/integ_test.py
@@ -1,19 +1,23 @@
 import sys
-from app.models.task_m import Task
-from app.reps.task_repo import TaskRepo
-import uuid
 import pytest
+import uuid
+from pathlib import Path
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+
 sys.path.append(str(BASE_DIR))
-sys.path.append(str(BASE_DIR / 'services/app'))
-#sys.path.append(str(BASE_DIR / 'movies_service/app'))
+sys.path.append(str(BASE_DIR / 'app/services'))
+
+from app.models.task_m import Task
+from app.reps.task_repo import TaskRepo
+
 def test_task_repo():
- repo = TaskRepo()
- tasks = repo.get_tasks()
- assert isinstance(tasks, list)
- task = repo.create_task(Task(id=str(uuid.uuid4()), name='test', task_user=str(uuid.uuid4())))
- assert isinstance(task.id, uuid.UUID)
- assert character.name == 'test'
- assert isinstance(task.task_user, uuid.UUID)
+  repo = TaskRepo()
+  tasks = repo.get_tasks()
+  assert isinstance(tasks, list)
+  task = repo.create_task(Task(id=str(uuid.uuid4()), name='test', task_user=str(uuid.uuid4())))
+  assert isinstance(task.id, uuid.UUID)
+  assert task.name == 'test'
+  assert isinstance(task.task_user, uuid.UUID)
