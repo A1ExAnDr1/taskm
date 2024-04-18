1bc2c23778aef7749e7521dceb5237266234e6a0
Alexandr <93984557+A1ExAnDr1@users.noreply.github.com>
2024-04-18 21:57:44 +0300

    tests 1

--- tests/unit/test_task_model.py
+++ tests/unit/test_task_model.py
@@ -7,11 +7,12 @@ from app.models.task_m import Task
 from app.models.user_m import User
 import uuid
 
+
 def test_task_model():
- task = Task(id=str(uuid.uuid4()), name='test',task_user=str(uuid.uuid4()))
+ task = Task(id=str(uuid.uuid4()), name='test', task_user=str(uuid.uuid4()))
  assert isinstance(task.id, uuid.UUID)
  assert task.name == 'test'
- assert isinstance(task_user.id, uuid.UUID)
+ assert isinstance(task.task_user, uuid.UUID)
 
 
 def test_user_model():
