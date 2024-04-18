66ab8ea28cbb28af94486d500a134792f1bd484b
Alexandr <93984557+A1ExAnDr1@users.noreply.github.com>
2024-04-18 19:40:17 +0300

    ci/cd+

--- tests/integ/integ_test.py
+++ tests/integ/integ_test.py
@@ -14,9 +14,11 @@ from fastapi import Depends
 from app.models.task_m import Task
 from app.reps.task_repo import TaskRepo
 from app.services.task_serv import TaskServ
+from sqlalchemy.orm import Session
+from app.database import get_db
 def test_task_repo():
   repo = Annotated[TaskServ, Depends(TaskServ)]
-  tasks = repo.get_tasks()
+  tasks = repo.get_tasks(self)
   assert isinstance(tasks, list)
   task = repo.create_task(Task(id=str(uuid.uuid4()), name='test', task_user=str(uuid.uuid4())))
   assert isinstance(task.id, uuid.UUID)
