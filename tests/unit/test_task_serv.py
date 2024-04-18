956b40368e1604f8a7660995761f221eb0005e98
Alexandr <93984557+A1ExAnDr1@users.noreply.github.com>
2024-04-18 21:43:59 +0300

    tests 1

--- tests/unit/test_task_serv.py
+++ tests/unit/test_task_serv.py
@@ -5,11 +5,11 @@ import uuid
 def test_task_model():
  task = Task(id=str(uuid.uuid4()), name='test',task_user=str(uuid.uuid4()))
  assert isinstance(task.id, uuid.UUID)
- assert character.name == 'test'
+ assert task.name == 'test'
  assert isinstance(task_user.id, uuid.UUID)
 
 
 def test_user_model():
  user = User(id=str(uuid.uuid4()), name='test')
  assert isinstance(user.id, uuid.UUID)
- assert equipment.name == 'test'
+ assert user.name == 'test'
