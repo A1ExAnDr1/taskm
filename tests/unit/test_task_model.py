af5cbf0eb1d4a7462af0ec10d81879d82a075bbb
Alexandr <93984557+A1ExAnDr1@users.noreply.github.com>
2024-04-18 21:49:03 +0300

    tests 1

--- tests/unit/test_task_model.py
+++ tests/unit/test_task_model.py
@@ -4,16 +4,22 @@ from app.models.task_m import Task
 import uuid
 from unittest.mock import Mock
 
+
 def test_task_service():
  repo = Mock(spec=TaskRepo)
  service = TaskServ(repo)
 
  task = Task(id=str(uuid.uuid4()), name='test', task_user=str(uuid.uuid4()))
 
+ # Mocking the behavior of the repo
  repo.create_task.return_value = task
- created_task = service.create_task(task)
+ repo.get_tasks.return_value = [task]
 
- repo.get_tasks.return_value = [created_task]
+ # Testing create_task method
+ created_task = service.create_task(task)
+ assert created_task == task
+
+ # Testing get_tasks method
  tasks = service.get_tasks()
 
  assert len(tasks) == 1
