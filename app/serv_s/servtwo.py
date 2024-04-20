509c143967f20ed8865c8e281a41671839b52a9d
Alexandr <93984557+A1ExAnDr1@users.noreply.github.com>
2024-04-20 19:31:16 +0300

    new br(3)

--- app/serv_s/servtwo.py
+++ app/serv_s/servtwo.py
@@ -1,46 +1,23 @@
 import os
 import uvicorn
-from fastapi import FastAPI, status, Query, HTTPException, Form, Header
+from fastapi import FastAPI, status, Query, HTTPException, Form, Header , Body
 import requests
 import random
-from keycloak import KeycloakOpenID
+from reqest import auth
+from sets import settings
+
+#from keycloak import KeycloakOpenID
 
 app = FastAPI()
 
-KEYCLOAK_URL = "http://keycloak:8080/"
-KEYCLOAK_CLIENT_ID = "testClient"
-KEYCLOAK_REALM = "testRealm"
-KEYCLOAK_CLIENT_SECRET = "**********"
+@app.post('/auth')
+def auth_user(username: str = Body(), password: str = Body()):
+    user = auth(username=username, password=password)
+    print(user)
+    if (user.status_code != 200):
+        raise HTTPException(401, 'bad user data')
+    return user.json()['access_token']
 
-keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
-                                  client_id=KEYCLOAK_CLIENT_ID,
-                                  realm_name=KEYCLOAK_REALM,
-                                  client_secret_key=KEYCLOAK_CLIENT_SECRET)
-
-from prometheus_fastapi_instrumentator import Instrumentator
-Instrumentator().instrument(app).expose(app)
-
-@app.post("/get_jwt_token")
-async def login(username: str = Form(...), password: str = Form(...)):
-    try:
-        token = keycloak_openid.token(grant_type=["password"],
-                                      username=username,
-                                      password=password)
-        global user_token
-        user_token = token
-        return token
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=400, detail="Не удалось получить токен")
-
-def chech_for_role_test(token):
-    try:
-        token_info = keycloak_openid.introspect(token)
-        if "test" not in token_info["realm_access"]["roles"]:
-            raise HTTPException(status_code=403, detail="Access denied")
-        return token_info
-    except Exception as e:
-        raise HTTPException(status_code=401, detail="Invalid token or access denied")
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def service_alive():
     return {'message': 'service alive'}
@@ -52,24 +29,6 @@ async def get_random_movie():
     r = requests.get(f"https://kinopoiskapiunofficial.tech/api/v2.2/films/{r_id}", headers={'X-API-KEY': '700211e1-f970-499f-9957-6bca24e2adb1'})
     return r.json()
 
-
-@app.get("/search_by_title")
-async def search_by_title(title: str):
-    r = requests.get(
-        f"https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword={title}",
-        headers={'X-API-KEY': '700211e1-f970-499f-9957-6bca24e2adb1'}
-    )
-    return r.json()
-
-
-@app.get("/get_movie_by_id")
-async def get_movie_by_id(q: list | None = Query()):
-    film_list = []
-    for id in q:
-        r = requests.get(f"https://kinopoiskapiunofficial.tech/api/v2.2/films/{id}",
-                         headers={'X-API-KEY': '700211e1-f970-499f-9957-6bca24e2adb1'})
-        film_list.append(r.json())
-    return film_list
 
 
 if __name__ == "__main__":
